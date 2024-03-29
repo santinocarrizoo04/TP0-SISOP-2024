712fd08dd2b7b599e4691e0605ca963de5177d4a
santinocarrizoo04 <sacarrizo@frba.utn.edu.ar>
2024-03-29 14:08:05 +0000

    Resolucion TP0

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # TP0-SISOP-2024
+
+## ¿De qué trata?
+
+El TP0 es una práctica inicial para empezar a familiarizarse con algunas de las herramientas necesarias para el trabajo práctico cuatrimestral de la materia Sistemas Operativos.
+
+Es un ejercicio corto y sirve como base para empezar el TP cuatrimestral.
+
+## Consigna
+
+Podés encontrar la consigna en el siguiente [link].
+
+[link]: https://faq.utnso.com.ar/tp0-enunciado
---
+++ client/src/client.c
@@ -0,0 +1,117 @@
+#include "client.h"
+
+int main(void)
+{
+	
+	int conexion;
+	char* ip;
+	char* puerto;
+	char* valor;
+
+	/*---------------------------------------------------PARTE 2-------------------------------------------------------------*/
+
+	t_log* logger;
+	t_config* config;
+
+	/* ---------------- LOGGING ---------------- */
+
+	logger = iniciar_logger();
+	log_info(logger, "Hola! Este es el inicio del programa-------");
+
+	/* ---------------- ARCHIVOS DE CONFIGURACION ---------------- */
+
+	config = iniciar_config();
+
+	ip = config_get_string_value(config, "IP");
+	puerto = config_get_string_value(config, "PUERTO");
+	valor = config_get_string_value(config, "CLAVE");
+
+	log_info(logger, "Cargando config... El valor de la clave es : %s", valor);
+
+	/* --------------- LEER DE CONSOLA ---------------- */
+
+	leer_consola(logger);
+
+	/*---------------------------------------------------PARTE 3-------------------------------------------------------------*/
+
+	// ADVERTENCIA: Antes de continuar, tenemos que asegurarnos que el servidor esté corriendo para poder conectarnos a él
+
+	// Creamos una conexión hacia el servidor
+	conexion = crear_conexion(ip, puerto);
+
+	// Enviamos al servidor el valor de CLAVE como mensaje
+	enviar_mensaje(valor, conexion;)
+	// Armamos y enviamos el paquete
+	paquete(conexion);
+
+	terminar_programa(conexion, logger, config);
+
+	/*---------------------------------------------------PARTE 5-------------------------------------------------------------*/
+	// Proximamente
+}
+
+t_log* iniciar_logger(void)
+{
+	t_log* nuevo_logger;
+	nuevo_logger = log_create("tp0.log", "TP0", 1, LOG_LEVEL_INFO);
+	if (nuevo_logger == NULL){
+		printf("No se pudo crear el LOG\n");
+		exit(1);
+	}
+	else{
+		return nuevo_logger;
+	}
+}
+
+t_config* iniciar_config(void)
+{
+	t_config* nuevo_config;
+	nuevo_config = config_create("cliente.config");
+	if(nuevo_config == NULL){
+		printf("No pudo crearse el config\n");
+		exit(1);
+	}else return nuevo_config;
+}
+
+void leer_consola(t_log* logger)
+{
+	char* leido;
+
+	leido = readline("> ");
+	while(!string_is_empty(leido)){
+		log_info(logger, "Lo leido por la consola es: %s", leido);
+		leido = readline("> ");
+	}
+
+	free(leido);
+}
+
+void paquete(int conexion)
+{
+	// Ahora toca lo divertido!
+	char* leido;
+	t_paquete* paquete = crear_paquete();
+
+	// Leemos y esta vez agregamos las lineas al paquete
+	leido = readline("> ")
+	while(!string_is_empty(leido)){
+		agregar_a_paquete(paquete, leido, strlen(leido) + 1);
+		leido = readline("> ")
+	}
+
+	enviar_paquete(paquete, conexion);
+	// ¡No te olvides de liberar las líneas y el paquete antes de regresar!
+	free(leido);
+	eliminar_paquete(paquete);	
+}
+
+void terminar_programa(int conexion, t_log* logger, t_config* config)
+{
+	if(logger != NULL){
+		log_destroy(logger);
+	}
+	if(config != NULL){
+		config_destroy(config);
+	}
+	liberar_conexion(conexion);
+}
